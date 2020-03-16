f2b98c0cde6512895dc577070d563100f35dd893
Roman Yakovlev <tensegrity666@eicloud.com>
2020-03-16 12:36:08 +0300

    feat: add screen toggler

--- source/scripts/Modal.js
+++ source/scripts/Modal.js
@@ -1,12 +1,13 @@
 const modal = document.querySelector('.modal__wrapper');
-const modalContent = modal.querySelector('.modal__content');
+const modalWindow = modal.querySelector('.modal__content');
+const modalContent = modalWindow.querySelector('.modal__wrapper');
 const button = document.body.querySelector('#submit-button');
 
 export const modalShow = () => {
   button.addEventListener('click', (event) => {
     event.preventDefault();
-    modal.classList.add('modal__wrapper_show');
-    modalContent.classList.add('modal__content_show');
     document.body.classList.add('page_noscroll');
+    modal.classList.add('modal__wrapper_show');
+    modalWindow.classList.add('modal__content_show');
   })
 }
--- source/scripts/script.js
+++ source/scripts/script.js
@@ -2,10 +2,12 @@ import { arrows } from './Slider.js';
 import { tabs } from './Tabs.js';
 import { portfolio } from './Portfolio.js';
 import { modalShow } from './Modal.js';
+import { screen } from './Screen.js';
 
 window.onload = () => {
   arrows();
   tabs();
   portfolio();
   modalShow();
+  screen();
 };
