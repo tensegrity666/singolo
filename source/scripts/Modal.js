77f5f9a34f4afd97084218501072c4a7b81bb82a
Roman Yakovlev <tensegrity666@eicloud.com>
2020-03-16 17:51:07 +0300

    feat: add form

--- source/scripts/Modal.js
+++ source/scripts/Modal.js
@@ -1,13 +1,58 @@
 const modal = document.querySelector('.modal__wrapper');
 const modalWindow = modal.querySelector('.modal__content');
 const modalContent = modalWindow.querySelector('.modal__wrapper');
-const button = document.body.querySelector('#submit-button');
+const submit = document.body.querySelector('#submit-button');
+const close = modalWindow.querySelector('.modal__close');
 
 export const modalShow = () => {
-  button.addEventListener('click', (event) => {
-    event.preventDefault();
-    document.body.classList.add('page_noscroll');
-    modal.classList.add('modal__wrapper_show');
-    modalWindow.classList.add('modal__content_show');
-  })
+  isValid();
+}
+
+  close.addEventListener('click', () => {
+    noScroll();
+    hide();
+  });
+
+const noScroll = () => {
+  document.body.classList.toggle('page_noscroll');
+}
+
+const show = () => {
+  modal.classList.add('modal__wrapper_show');
+  modalWindow.classList.add('modal__content_show');
+}
+
+const hide = () => {
+  modal.classList.remove('modal__wrapper_show');
+  modalWindow.classList.remove('modal__content_show');
+}
+
+const isValid = () => {
+  submit.addEventListener('click', (event) => {
+    if (name.validity.valid && email.validity.valid) {
+      event.preventDefault();
+      noScroll();
+      show();
+      dataToModal();
+      form.submit();
+    }
+  });
+}
+
+const form = document.querySelector('.contacts__form');
+const name = form.querySelector('#Name');
+const email = form.querySelector('#Email');
+const subject = form.querySelector('#Subject');
+const comment = form.querySelector('#Comment');
+
+const nameContent = document.querySelector('#nameContent');
+const emailContent = document.querySelector('#emailContent');
+const subjectContent = document.querySelector('#subjectContent');
+const commentContent = document.querySelector('#commentContent');
+
+const dataToModal = () => {
+  nameContent.innerText = `Name: ${name.value}`;
+  emailContent.innerText = `E-mail: ${email.value}`;
+  subjectContent.innerText = `Title: ${subject.value}`;
+  commentContent.innerText = `Message: ${comment.value}`;
 }
