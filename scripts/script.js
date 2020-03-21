5e47405d321294ea44eca2bd4afe7f2481e1f105
Roman Yakovlev <tensegrity666@eicloud.com>
2020-03-22 00:25:11 +0300

    fix: minor fixes

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+import { arrows } from './Slider.js';
+import { navigation } from './Navigation.js';
+import { portfolio } from './Portfolio.js';
+import { modalShow } from './Modal.js';
+import { screen } from './Screen.js';
+
+window.onload = () => {
+  arrows();
+  navigation();
+  portfolio();
+  modalShow();
+  screen();
+};
