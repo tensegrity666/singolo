a712c7856b03dd10af33e9c00c1a514794e09425
Roman Yakovlev <tensegrity666@eicloud.com>
2020-03-27 17:34:02 +0300

    fix: minor fixes

--- source/scripts/Menu.js
+++ source/scripts/Menu.js
@@ -4,19 +4,12 @@ const header = document.querySelector('.header__main');
 const nav = header.querySelector('.navigation-main');
 const burger = nav.querySelector('.navigation-main__button-inner');
 const logo = header.querySelector('.logo__item');
-const overlay = document.querySelector('.modal__wrapper');
 
 const showMenu = () => {
   nav.classList.toggle('navigation-main_show');
   burger.classList.toggle('navigation-main__button-inner_reverse');
   logo.classList.toggle('logo__item_menu');
   modal.classList.toggle('modal__wrapper_show');
-}
-
-const closeOnBlur = () => {
-  overlay && overlay.addEventListener('click', () => {
-    showMenu()
-  });
 }
 
 export const menuHandler = () => {
@@ -29,5 +22,4 @@ export const menuHandler = () => {
   burger.addEventListener('touchmove', event => {
     event.preventDefault();
   });
-  closeOnBlur();
 }
